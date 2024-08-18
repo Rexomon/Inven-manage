633ba9ad8be6bfc2ded98571f635f00fe7b078a0
Rexomon <muhammadindriyo12@gmail.com>
2024-08-18 18:02:23 +0700

    tsconfig update

--- inv-fr/env.d.ts
+++ inv-fr/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly BASE_URL: string;
+    // tambahkan variabel lingkungan lainnya di sini jika diperlukan
+  }
+
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
