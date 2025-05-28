ec22d940b8a7b4c5ac6c6cc2fd275cad6737fd74
Rexomon <muhammadindriyo12@gmail.com>
2025-05-28 21:24:52 +0700

    Add biome configuration file

--- inv-bk/src/Config/Redis.ts
+++ inv-bk/src/Config/Redis.ts
@@ -2,25 +2,25 @@ import Redis from "ioredis";
 
 const RedisPort = process.env.REDIS_PORT;
 
-if(!RedisPort) {
-  console.error("REDIS_PORT is not set in environment variables");
-  process.exit(1);
+if (!RedisPort) {
+	console.error("REDIS_PORT is not set in environment variables");
+	process.exit(1);
 }
 
 const redis = new Redis(RedisPort, {
-  maxRetriesPerRequest: null,
-  enableReadyCheck: true,
-  connectTimeout: 10000,
+	maxRetriesPerRequest: null,
+	enableReadyCheck: true,
+	connectTimeout: 10000,
 });
 
 export const safelyCloseRedis = async () => {
-  try {
-    await redis.quit();
-    console.log("Redis connection closed safely.");
-  } catch (error) {
-    console.error("Error closing Redis connection:", error);
-  }
-}
+	try {
+		await redis.quit();
+		console.log("Redis connection closed safely.");
+	} catch (error) {
+		console.error("Error closing Redis connection:", error);
+	}
+};
 
 redis.on("connect", () => {
 	console.log("Redis Connected");
